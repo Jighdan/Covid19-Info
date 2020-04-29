4293453b30cc6c33aa9e58d38e7c8ab003964733
Reinny <reynsth@gmail.com>
2020-04-29 14:28:36 -0400

    CSS Modulation

--- assets/scripts/translate.js
+++ assets/scripts/translate.js
@@ -2,19 +2,19 @@
 const translation = {
   english:
     {
-      "what": ["What is it?",
+      what: ["What is it?",
             "To understand what COVID-19 is we must define what a coronavirus is first; coronaviruses are a large family of viruses which may cause illness in animals or humans. In humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS).\n\nCOVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019."],
-      "how": ["How does it spread?", 
+      how: ["How does it spread?", 
             "People can catch COVID-19 from others who have the virus. The disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. People can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. This is why it is important to stay more than 1 meter (3 feet) away from a person who is sick."],
-      "symptoms": ["What are the symptoms?",
+      symptoms: ["What are the symptoms?",
             "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don’t develop any symptoms and don't feel unwell. Most people (about 80%) recover from the disease without needing special treatment. Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. Older people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. People with fever, cough and difficulty breathing should seek medical attention.\n\nWhile we are still learning about how COVID-2019 affects people, older persons and persons with pre-existing medical conditions (such as high blood pressure, heart disease, lung disease, cancer or diabetes) appear to develop serious illness more often than others."],
-      "prevention": ["Prevention & Care", 
+      prevention: ["Prevention & Care", 
             ["Regularly and thoroughly clean your hands with an alcohol-based hand rub or wash them with soap and water.", 
             "Maintain at least 1 metre (3 feet) distance between yourself and anyone who is coughing or sneezing.", 
             "Avoid touching eyes, nose and mouth.",
             "Make sure you, and the people around you, follow good respiratory hygiene. This means covering your mouth and nose with your bent elbow or tissue when you cough osneeze. Then dispose of the used tissue immediately.",
             "Stay home if you feel unwell. If you have a fever, cough and difficulty breathing, seek medical attention and call in advance. Follow the directions of your locahealth authority."]],
-      "faq": ["Questions & Answers", 
+      faq: ["Questions & Answers", 
             ["How likely an I to catch COVID-19", 
                   "The risk depends on where you are - and more specifically, whether there is a COVID-19 outbreak unfolding there. For most people in most locations the risk of catching COVID-19 is still low. However, there are now places around the world (cities or areas) where the disease is spreading. For people living in, or visiting, these areas the risk of catching COVID-19 is higher. Governments and health authorities are taking vigorous action every time a new case of COVID-19 is identified. Be sure to comply with any local restrictions on travel, movement or large gatherings. Cooperating with disease control efforts will reduce your risk of catching or spreading COVID-19. COVID-19 outbreaks can be contained and transmission stopped, as has been shown in China and some other countries. Unfortunately, new outbreaks can emerge rapidly. It’s important to be aware of the situation where you are or intend to go."],
             ["Is there a vaccine, drug or tratment for COVID-19", 
@@ -28,24 +28,24 @@ const translation = {
             ["How did the fist human SARS-CoV-2 infections occur?",
                   "The first human cases of COVID-19 were identified in Wuhan City, China in December 2019. At this stage, it is not possible to determine precisely how humans in China were initially infected with SARS-CoV-2. \n\nHowever, SARS-CoV, the virus which caused the SARS outbreak in 2003, jumped from an animal reservoir (civet cats, a farmed wild animal) to humans and then spread between humans. In a similar way, it is thought that SARS-CoV-2 jumped the species barrier and initially infected humans, but more likely through an intermediate host, that is another animal species more likely to be handled by humans - this could be a domestic animal, a wild animal, or a domesticated wild animal and, as of yet, has not been identified. \n\nUntil the source of this virus is identified and controlled, there is a risk of reintroduction of the virus in the human population and the risk of new outbreaks like the ones we are currently experiencing."]
               ],
-      "about": "Read more" 
+      about: "Read more" 
   },
 
-  "spanish":
+  spanish:
     {
-      "what": ["¿Que es un COVID-19?",
+      what: ["¿Que es un COVID-19?",
             "Los coronavirus son una extensa familia de virus que pueden causar enfermedades tanto en animales como en humanos. En los humanos, se sabe que varios coronavirus causan infecciones respiratorias que pueden ir desde el resfriado común hasta enfermedades más graves como el síndrome respiratorio de Oriente Medio (MERS) y el síndrome respiratorio agudo severo (SRAS). El coronavirus que se ha descubierto más recientemente causa la enfermedad por coronavirus COVID-19.\n\nLa COVID-19 es la enfermedad infecciosa causada por el coronavirus que se ha descubierto más recientemente. Tanto el nuevo virus como la enfermedad eran desconocidos antes de que estallara el brote en Wuhan (China) en diciembre de 2019."],
-      "how": ["¿Como se propaga?", 
+      how: ["¿Como se propaga?", 
             "Una persona puede contraer la COVID-19 por contacto con otra que esté infectada por el virus. La enfermedad puede propagarse de persona a persona a través de las gotículas procedentes de la nariz o la boca que salen despedidas cuando una persona infectada tose o exhala. Estas gotículas caen sobre los objetos y superficies que rodean a la persona, de modo que otras personas pueden contraer la COVID-19 si tocan estos objetos o superficies y luego se tocan los ojos, la nariz o la boca. También pueden contagiarse si inhalan las gotículas que haya esparcido una persona con COVID-19 al toser o exhalar. Por eso es importante mantenerse a más de 1 metro (3 pies) de distancia de una persona que se encuentre enferma."],
-      "symptoms": ["¿Cuales son los sintomas?",
+      symptoms: ["¿Cuales son los sintomas?",
             "Los síntomas más comunes de la COVID-19 son fiebre, cansancio y tos seca. Algunos pacientes pueden presentar dolores, congestión nasal, rinorrea, dolor de garganta o diarrea. Estos síntomas suelen ser leves y aparecen de forma gradual. Algunas personas se infectan pero no desarrollan ningún síntoma y no se encuentran mal. La mayoría de las personas (alrededor del 80%) se recupera de la enfermedad sin necesidad de realizar ningún tratamiento especial. Alrededor de 1 de cada 6 personas que contraen la COVID-19 desarrolla una enfermedad grave y tiene dificultad para respirar. Las personas mayores y las que padecen afecciones médicas subyacentes, como hipertensión arterial, problemas cardiacos o diabetes, tienen más probabilidades de desarrollar una enfermedad grave. En torno al 2% de las personas que han contraído la enfermedad han muerto. Las personas que tengan fiebre, tos y dificultad para respirar deben buscar atención médica."],
-      "prevention": ["Prevencion & Cuidado", 
+      prevention: ["Prevencion & Cuidado", 
             ["Lávese las manos a fondo y con frecuencia usando un desinfectante a base de alcohol o con agua y jabón.", 
             "Mantenga una distancia mínima de 1 metro (3 pies) entre usted y cualquier persona que tosa o estornude.", 
             "Evite tocarse los ojos, la nariz y la boca.",
             "Tanto usted como las personas que les rodean deben asegurarse de mantener una buena higiene de las vías respiratorias. Eso significa cubrirse la boca y la nariz con el codo doblado o con un pañuelo de papel al toser o estornudar. El pañuelo usado debe desecharse de inmediato.",
             "Permanezca en casa si no se encuentra bien. Si tiene fiebre, tos y dificultad para respirar, busque atención médica y llame con antelación. Siga las instrucciones dlas autoridades sanitarias locales."]],
-      "faq": ["Preguntas & Respuestas", 
+      faq: ["Preguntas & Respuestas", 
             ["¿Qué probabilidades hay de que contraiga la COVID-19?", 
                   "El riesgo depende del lugar donde se encuentre usted y, más concretamente, de si se está produciendo un brote de COVID-19 en dicho lugar.\n\nPara la mayoría de las personas que se encuentran en la mayor parte de los lugares, el riesgo de contraer esta enfermedad continúa siendo bajo. Sin embargo, sabemos que hay algunos lugares (ciudades o zonas) donde se está propagando y donde el riesgo de contraerla es más elevado, tanto para las personas que viven en ellas como para las que las visitan. Los gobiernos y las autoridades sanitarias están actuando con determinación cada vez que se detecta un nuevo caso de COVID-19. Es importante que todos respetemos las restricciones relativas a los viajes, los desplazamientos y las concentraciones multitudinarias de personas aplicables a cada lugar en concreto. Si cooperamos con las medidas de lucha contra la enfermedad, reduciremos el riesgo que corremos cada uno de nosotros de contraerla o de propagarla."],
             ["¿Existe alguna vacuna, medicamento o tratamiento para la COVID-19?", 
@@ -59,24 +59,24 @@ const translation = {
             ["¿Cómo se produjeron las primeras infecciones humanas por el SARS-CoV-2?",
                   "Los primeros casos humanos de COVID-19 se detectaron en la ciudad de Wuhan, China, en diciembre de 2019. Por el momento, no es posible determinar con precisión cómo se infectaron las primeras personas en China por el SARS-CoV-2.\n\nA pesar de ello, el virus del SARS-CoV, que causó el brote de SARS en 2003, pasó de un reservorio animal (la civeta, un animal silvestre de granja) a los seres humanos para luego propagarse entre ellos. De manera similar, se piensa que el SARS-CoV-2 cruzó la barrera entre especies e infectó en un primer momento a seres humanos, si bien muy probablemente a través de un huésped intermedio, es decir, otra especie animal más manipulada por el ser humano (por ejemplo un animal doméstico, un animal silvestre o un animal silvestre domesticado, el cual no ha sido identificado hasta la fecha).\n\nHasta que se localice y controle la fuente de este virus, existe el riesgo de que vuelva a pasar a la población humana y de que se produzcan nuevos brotes como los que estamos sufriendo actualmente."]
               ],
-      "about": "Read more" 
+      about: "Read more" 
   },
 
-  "filipino":
+  filipino:
     {
-      "what": ["Ano yun?",
+      what: ["Ano yun?",
             "Upang maunawaan kung ano ang COVID-19 ay dapat nating tukuyin kung ano ang una sa isang coronavirus; Ang mga coronavirus ay isang malaking pamilya ng mga virus na maaaring magdulot ng sakit sa mga hayop o tao. Sa mga tao, ang ilang mga coronavirus ay kilala upang maging sanhi ng impeksyon sa paghinga mula sa karaniwang sipon hanggang sa mas malubhang sakit tulad ng Middle East Respiratory Syndrome (MERS) at Severe Acute Respiratory Syndrome (SARS).\n\nCOVID-19 ay ang nakakahawang sakit na dulot ng impeksyon ang pinakahuling natuklasan na coronavirus. Ang bagong virus at sakit na ito ay hindi alam bago magsimula ang pagsiklab sa Wuhan, China, noong Disyembre 2019."],
-      "how": ["Paano ito kumalat?", 
+      how: ["Paano ito kumalat?", 
             "Maaaring mahuli ng mga tao ang COVID-19 mula sa iba na may virus. Ang sakit ay maaaring kumalat mula sa bawat tao sa pamamagitan ng maliit na mga patak mula sa ilong o bibig na kumakalat kapag ang isang taong may COVID-19 na ubo o huminga. Ang mga droplet na ito ay lumapag sa mga bagay at ibabaw sa paligid ng tao. Ang ibang mga tao pagkatapos ay mahuli ang COVID-19 sa pamamagitan ng pagpindot sa mga bagay o ibabaw na ito, pagkatapos ay hawakan ang kanilang mga mata, ilong o bibig. Maaari ring mahuli ng mga tao ang COVID-19 kung huminga sila sa mga patak mula sa isang taong may COVID-19 na ubo o humihinga ng mga patak. Ito ang dahilan kung bakit mahalagang manatiling higit sa 1 metro (3 piye) ang layo mula sa isang taong may sakit."],
-      "symptoms": ["Ano ang mga sintomas ng covid-19?",
+      symptoms: ["Ano ang mga sintomas ng covid-19?",
             "Ang pinakakaraniwang sintomas ng COVID-19 ay lagnat, pagkapagod, at tuyong ubo. Ang ilang mga pasyente ay maaaring magkaroon ng pananakit at pananakit, kasikipan ng ilong, matipid na ilong, namamagang lalamunan o pagtatae. Ang mga sintomas na ito ay karaniwang banayad at nagsisimula nang unti-unti. Ang ilang mga tao ay nahawahan ngunit hindi nagkakaroon ng anumang mga sintomas at hindi nakakaramdam ng hindi malusog. Karamihan sa mga tao (tungkol sa 80%) ay nakabawi mula sa sakit nang hindi nangangailangan ng espesyal na paggamot. Halos 1 sa bawat 6 na tao na nagkakaroon ng COVID-19 ay nagkasakit ng malubha at nahihirapan sa paghinga. Ang mga matatandang tao, at ang mga may kalakip na mga problemang medikal tulad ng mataas na presyon ng dugo, mga problema sa puso o diyabetis, ay mas malamang na magkaroon ng malubhang sakit. \n\nAng mga taong may lagnat, ubo at kahirapan sa paghinga ay dapat humingi ng medikal na atensyon. ang sakit, cancer o diabetes) ay lilitaw na magkaroon ng malubhang sakit nang mas madalas kaysa sa iba."],
-      "prevention": ["Pag-iwas & Pangangalaga", 
+      prevention: ["Pag-iwas & Pangangalaga", 
             ["Regular at lubusan linisin ang iyong mga kamay gamit ang isang kamay na nakabatay sa alkohol na kuskusin o hugasan mo sila ng sabon at tubig.", 
             "Panatilihin ang hindi bababa sa 1 metro (3 talampakan) ang distansya sa pagitan ng iyong sarili at sinumang ubo o pagbahing.", 
             "Aiwasang hawakan ang mga mata, ilong at bibig.",
             "Tiyaking ikaw, at ang mga tao sa paligid mo, sundin ang mahusay na kalinisan sa paghinga. Nangangahulugan ito na takpan ang iyong bibig at ilong gamit ang iyonbaluktot na siko o tisyu kapag umubo ka o bumahin. Pagkatapos ay itapon agad ang ginamit na tisyu.",
             "Manatili sa bahay kung sa tingin mo ay hindi maayos. Kung mayroon kang lagnat, ubo at kahirapan sa paghinga, humingi ng medikal na atensyon at tumawag nang maaga. Sundin ang mga direksyon ng iyong lokal na awtoridad sa kalusugan."]],
-      "faq": ["Mga tanong at mga Sagot", 
+      faq: ["Mga tanong at mga Sagot", 
             ["Paano malamang ang isang I ay mahuli ang COVID-19?", 
                   "Panganib siya ay nakasalalay sa kung nasaan ka - at mas partikular, kung mayroong isang pag-aalsa ng COVID-19. Para sa karamihan ng mga tao sa karamihan ng mga lokasyon ang panganib na mahuli ang COVID-19 ay mababa pa rin. Gayunpaman, may mga lugar na ngayon sa buong mundo (mga lungsod o lugar) kung saan kumakalat ang sakit. Para sa mga taong naninirahan, o pagbisita, ang mga lugar na ito ang panganib na mahuli ang COVID-19 ay mas mataas. Ang mga gobyerno at awtoridad sa kalusugan ay nagsasagawa ng masigasig na pagkilos sa tuwing natukoy ang isang bagong kaso ng COVID-19. Siguraduhing sumunod sa anumang lokal na mga paghihigpit sa paglalakbay, kilusan o malaking pagtitipon. Ang pakikipagtulungan sa mga pagsisikap sa pagkontrol sa sakit ay mabawasan ang iyong panganib sa paghuli o pagkalat ng COVID-19. Ang mga COVID-19 na pag-iwas ay maaaring nilalaman at itigil ang paghahatid, tulad ng ipinakita sa Tsina at ilang iba pang mga bansa. Sa kasamaang palad, ang mga bagong pagsiklab ay maaaring lumitaw nang mabilis. Mahalagang malaman ang sitwasyon kung nasaan ka o balak mong puntahan."],
             ["Mayroon bang bakuna, gamot o paggamot para sa COVID-19?", 
@@ -90,7 +90,9 @@ const translation = {
             ["Paano nangyari ang unang impeksyon ng SARS-CoV-2?",
                   "Ang mga unang kaso ng tao ng COVID-19 ay nakilala sa Wuhan City, China noong Disyembre 2019. Sa yugtong ito, hindi posible na matukoy nang eksakto kung paano ang mga tao sa Tsina ay unang nahawaan ng SARS-CoV-2. Gayunpaman, ang SARS-CoV, ang virus na naging sanhi ng pagsiklab ng SARS noong 2003, ay tumalon mula sa isang imbakan ng hayop (civet cats, isang sinasaka na hayop na hayop) sa mga tao at pagkatapos ay kumalat sa pagitan ng mga tao. Sa katulad na paraan, naisip na ang SARS-CoV-2 ay tumalon sa species ng species at una na nahawa ang mga tao, ngunit mas malamang sa pamamagitan ng isang intermediate host, iyon ay isa pang species ng hayop na mas madaling hawakan ng mga tao - maaaring ito ay isang domestic hayop , isang mabangis na hayop, o isang tinaguriang ligaw na hayop at, hanggang ngayon ay hindi pa nakilala. Hanggang sa ang mapagkukunan ng virus na ito ay nakilala at kinokontrol, mayroong panganib ng muling paggawa ng virus sa populasyon ng tao at ang panganib ng mga bagong pagsiklab tulad ng mga kasalukuyang nararanasan natin."]
               ],
-      "about": "Magbasa pa" 
+      about: "Magbasa pa" 
   }
 };
 
+const sectionWhat = document.getElementById("sectionWhat");
+sectionWhat.querySelector("h1").textContent = translation.spanish.what[0];
